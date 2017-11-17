450ba243b323f4411be9f3875954d1100f7dfc90
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-11-17 13:49:19 +0100

    Terminado Ejercicio 3 Práctica 06

--- SEW/p6/Ejercicio-4/CalculadoraCientifica.js
+++ SEW/p6/Ejercicio-4/CalculadoraCientifica.js
@@ -1,4 +1,86 @@
 "use strict";
+
+class Calculadora {
+    constructor() {
+        this.pantalla = "0";
+        this.memoria = "0";
+        this.memoriaActual = "0";
+        this.esResultado = false;
+    }
+
+    actualizarPantalla() {
+        var p = document.getElementById("pantalla");
+        p.value = this.pantalla;
+    }
+
+    agregarDisplay(digito) {
+        if (this.esResultado) {
+            this.limpiarResultado();
+            this.pantalla = digito;
+        }
+        else if (this.pantalla == "0") {
+            this.pantalla = digito;
+        }
+        else {
+            this.pantalla += digito;
+        }
+        this.esResultado = false;
+        this.actualizarPantalla();
+    }
+
+    agregarPunto() {
+        if (!this.pantalla.includes(".")) {
+            this.pantalla += ".";
+        }
+        this.actualizarPantalla();
+    }
+
+    limpiarDisplay() {
+        this.pantalla = "0";
+        this.actualizarPantalla();
+    }
+
+    limpiarResultado() {
+        this.limpiarDisplay();
+        this.memoriaActual = "";
+    }
+
+    limpiarTodo() {
+        this.limpiarResultado();
+        this.memoria = "0";
+    }
+
+    operacion(operador) {
+        this.memoriaActual += this.pantalla + " " + operador;
+        this.limpiarDisplay();
+    }
+
+    mostrarResultado() {
+        this.memoriaActual += this.pantalla;
+        this.pantalla = eval(this.memoriaActual);
+        this.actualizarPantalla();
+        this.esResultado = true;
+    }
+
+    memoriaMas() {
+        this.memoria += "+" + this.pantalla;
+        this.memoria = eval(this.memoria);
+        this.limpiarDisplay();
+    }
+
+    memoriaMenos() {
+        this.memoria += "-" + this.pantalla;
+        this.memoria = eval(this.memoria);
+        this.limpiarDisplay();
+    }
+
+    memoriaMostrar() {
+        this.pantalla = this.memoria;
+        this.memoriaActual = "";
+        this.actualizarPantalla();
+    }
+}
+
 class CalculadoraCientifica extends Calculadora {
     constructor() {
         super();
---
+++ SEW/p7/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Bombilla {
+    constructor() {
+
+    }
+
+    mostrarImagen() {
+        $("#miBombilla").show();
+    }
+
+    ocultarImagen() {
+        $("#miBombilla").hide();
+    }
+}
+
+var bombilla = new Bombilla();
