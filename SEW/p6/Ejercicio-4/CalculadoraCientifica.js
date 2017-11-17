188a4734a3b366b5cb4998fd33f5a2f9463305b2
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-11-18 00:37:15 +0100

    Ejercicio 4 Práctica 6

--- SEW/p6/Ejercicio-4/CalculadoraCientifica.js
+++ SEW/p6/Ejercicio-4/CalculadoraCientifica.js
@@ -18,7 +18,7 @@ class Calculadora {
             this.limpiarResultado();
             this.pantalla = digito;
         }
-        else if (this.pantalla == "0") {
+        else if ("0" == this.pantalla) {
             this.pantalla = digito;
         }
         else {
@@ -81,4 +81,83 @@ class Calculadora {
     }
 }
 
-var calculadora = new Calculadora();
+class CalculadoraCientifica extends Calculadora {
+    constructor() {
+        super();
+    }
+
+    operacion(operador) {
+        this.pantalla += operador;
+        this.actualizarPantalla();
+    }
+
+    cambiarSigno() {
+        if ("+" == this.pantalla.substring(0, 1)) {
+            this.pantalla = "-" + this.pantalla.substring(1, this.pantalla.length);
+        } else if ("-" == this.pantalla.substring(0, 1)) {
+            this.pantalla = "+" + this.pantalla.substring(1, this.pantalla.length);
+        } else
+            this.pantalla = "+" + this.pantalla.substring(0, this.pantalla.length);
+        this.memoriaActual = this.pantalla;
+        this.actualizarPantalla();
+    }
+
+    factorial() {
+        var x = eval(this.pantalla);
+        x = parseInt(x, 10);
+        if (isNaN(x)) return 1;
+
+        if (x <= 0) return 1;
+        if (x > 170) return Infinity;
+        var y = 1;
+        for (var i = x; i > 0; i--) {
+            y *= i;
+        }
+        this.pantalla = y;
+        this.memoriaActual = this.pantalla;
+        this.actualizarPantalla();
+    }
+
+    limpiarDisplayParte() {
+        this.pantalla = this.pantalla.substring(0, this.pantalla.length - 1);
+        this.actualizarPantalla();
+    }
+
+    calcularMath(operador) {
+        var aux = eval(this.pantalla);
+        switch (operador) {
+            case "e":
+                aux = Math.exp(aux);
+                break;
+            case "log":
+                aux = Math.log(aux);
+                break;
+            case "x":
+                aux = Math.pow(10, aux);
+                break;
+            case "y":
+                aux = Math.pow(aux, this.memoria);
+                break;
+            case "raiz":
+                aux = Math.sqrt(10, aux);
+                break;
+            case "sin":
+                aux = Math.sin(10, aux);
+                break;
+            case "cos":
+                aux = Math.cos(10, aux);
+                break;
+            case "tan":
+                aux = Math.tan(10, aux);
+                break;
+            case "2":
+                aux = Math.pow(aux, 2);
+                break;
+        }
+        this.pantalla = aux;
+        this.memoriaActual = this.pantalla;
+        this.actualizarPantalla();
+    }
+}
+
+var calculadora = new CalculadoraCientifica();
